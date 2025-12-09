c6057de7a0d41846b3510e431f8d5b944f3f314a
oliulalam <mdoliulalamjoy@gmail.com>
2025-12-10 02:35:40 +0600

    New Product Section Add

--- lib/presentation/ui/screens/home_screen.dart
+++ lib/presentation/ui/screens/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:crafty_bay/presentation/ui/utils/app_colors.dart';
 import 'package:crafty_bay/presentation/ui/utils/assets_path.dart';
 import 'package:crafty_bay/presentation/ui/widgets/app_icon_button.dart';
 import 'package:crafty_bay/presentation/ui/widgets/home_banner_slider.dart';
+import 'package:crafty_bay/presentation/ui/widgets/horizontal_product_list_view.dart';
 import 'package:crafty_bay/presentation/ui/widgets/search_text_field.dart';
 import 'package:crafty_bay/presentation/ui/widgets/section_header.dart';
 import 'package:flutter/material.dart';
@@ -20,28 +21,36 @@ class _HomeScreenState extends State<HomeScreen> {
     return Scaffold(
       appBar: _buildAppBar(),
 
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16),
-        child: Column(
-          children: [
-            SizedBox(height: 16),
-            SearchTextField(textEditingController: TextEditingController()),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Column(
+            children: [
+              SizedBox(height: 16),
+              SearchTextField(textEditingController: TextEditingController()),
+        
+              SizedBox(height: 16),
+        
+              HomeBannerSlider(),
 
-            SizedBox(height: 16),
-
-            HomeBannerSlider(),
-
-            SizedBox(height: 16),
-
-            _buildCategoriesSection(),
-
-            SectionHeader(title: "Popular", onTap: () {}),
-            SizedBox(height: 140, child: _buildProductListView()),
-          ],
+              SizedBox(height: 16),
+              _buildCategoriesSection(),
+              SizedBox(height: 16),
+              _buildPopularSection(),
+              SizedBox(height: 16),
+              _buildNewProductSection(),
+              SizedBox(height: 16),
+              _buildSpecialProductSection(),
+              SizedBox(height: 16),
+        
+        
+            ],
+          ),
         ),
       ),
     );
   }
+// Main Section End Here
 
   Widget _buildCategoriesSection() {
     return Column(
@@ -50,12 +59,63 @@ class _HomeScreenState extends State<HomeScreen> {
 
         SizedBox(height: 8.0),
 
-        SizedBox(height: 120, child: _buildCategoryListView()),
+        SizedBox(height: 120, child: HorizontalCategoryListView()),
       ],
     );
   }
 
-  Widget _buildCategoryListView() {
+  Widget _buildPopularSection(){
+    return Column(
+      children: [
+        SectionHeader(title: "Popular", onTap: () {}),
+        SizedBox(height: 180, child: HorizontalProductListView()),
+      ],
+    );
+  }
+
+  Widget _buildSpecialProductSection(){
+    return Column(
+      children: [
+        SectionHeader(title: "Special", onTap: () {}),
+        SizedBox(height: 180, child: HorizontalProductListView()),
+      ],
+    );
+  }
+
+  Widget _buildNewProductSection(){
+    return Column(
+      children: [
+        SectionHeader(title: "New", onTap: () {}),
+        SizedBox(height: 180, child: HorizontalProductListView()),
+      ],
+    );
+  }
+
+
+
+
+  AppBar _buildAppBar() {
+    return AppBar(
+      backgroundColor: Colors.white,
+      title: SvgPicture.asset(AssetsPath.appLogoNav),
+      actions: [
+        AppBarIconButton(iconData: Icons.person, onTap: () {}),
+        SizedBox(width: 8),
+        AppBarIconButton(iconData: Icons.call, onTap: () {}),
+        SizedBox(width: 8),
+        AppBarIconButton(iconData: Icons.notifications, onTap: () {}),
+      ],
+    );
+  }
+}
+
+class HorizontalCategoryListView extends StatelessWidget {
+  const HorizontalCategoryListView({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
     return ListView.separated(
       scrollDirection: Axis.horizontal,
       itemCount: 10,
@@ -82,51 +142,8 @@ class _HomeScreenState extends State<HomeScreen> {
       separatorBuilder: (_, __) => const SizedBox(width: 8),
     );
   }
-
-  Widget _buildProductListView() {
-    return ListView.separated(
-      scrollDirection: Axis.horizontal,
-      itemCount: 10,
-      itemBuilder: (context, index) {
-        return Container(
-          width: 120,
-          child: Column(
-            children: [
-              Container(
-                width: 120,
-                height: 100,
-                padding: EdgeInsets.all(16),
-                decoration: BoxDecoration(
-                  color: AppColors.themeColor.withOpacity(0.1),
-                  borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(8.0),
-                      topRight: Radius.circular(8.0),
-                  ),
-                  image: DecorationImage(
-                    image: AssetImage(AssetsPath.dummyProductImage),
-                    fit: BoxFit.scaleDown,
-                  ),
-                ),
-              ),
-            ],
-          ),
-        );
-      },
-      separatorBuilder: (_, __) => const SizedBox(width: 8),
-    );
-  }
-
-  AppBar _buildAppBar() {
-    return AppBar(
-      backgroundColor: Colors.white,
-      title: SvgPicture.asset(AssetsPath.appLogoNav),
-      actions: [
-        AppBarIconButton(iconData: Icons.person, onTap: () {}),
-        SizedBox(width: 8),
-        AppBarIconButton(iconData: Icons.call, onTap: () {}),
-        SizedBox(width: 8),
-        AppBarIconButton(iconData: Icons.notifications, onTap: () {}),
-      ],
-    );
-  }
 }
+
+
+
+
